a730911d9000e98d0e035c22baa6038dff12993b
Mysa33 <tpdrn.france@gmail.com>
2018-08-19 05:52:30 +0200

    Ticket 27

--- src/app/widgets/news/news.component.ts
+++ src/app/widgets/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../../shared/services/api.service';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
+  widgetStatus:boolean = false;
+  dataUrl:string = "http://localhost:4200/assets/data/newsData.json";
+  newsData;
+  newsArray;
+  constructor(private _newsService:ApiService) { }
 
   ngOnInit() {
+    this.getPostsData(this.widgetStatus);
   }
-
+  getPostsData(widgetStatus){
+    this.widgetStatus = widgetStatus;
+    this._newsService.getData(this.dataUrl).subscribe(
+      data => { 
+        this.newsData = data;
+        this.resolveAfterdelay(this.widgetStatus,this.newsData);
+      },
+      err => console.error(err),
+      () => console.log('done loading posts')
+    );
+  }
+  resolveAfterdelay(widgetStatus,newsData):any {
+    this.widgetStatus = widgetStatus;
+    this.newsData = newsData;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.widgetStatus = true;
+        resolve('resolved');
+      }, 
+      500);
+    });
+  }
 }
