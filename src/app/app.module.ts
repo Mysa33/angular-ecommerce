d56d147187c985dca1cbe763f38c67393ae3bcc4
Mysa33 <tpdrn.france@gmail.com>
2018-08-29 07:53:52 +0200

    profil comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { PostsComponent } from './sidebar/posts/posts.component';
 import { BannerComponent } from './widgets/banner/banner.component';
 import { BreadcrumbComponent } from './widgets/breadcrumb/breadcrumb.component';
 
+
 const routes: Routes  = [
   {path :'', redirectTo : 'home', pathMatch:'full'},
   {path :'home', component: HomeComponent},
