5ea0b94135b93f71b6f6c0c884420dd1b42f6b60
Mysa33 <tpdrn.france@gmail.com>
2018-09-19 15:24:56 +0200

    Blog comp TU

--- src/app/blog/blog.component.spec.ts
+++ src/app/blog/blog.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { BlogComponent } from './blog.component';
-
+import {ApiService} from '../shared/services/api.service';
 describe('BlogComponent', () => {
   let component: BlogComponent;
   let fixture: ComponentFixture<BlogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BlogComponent ]
+      declarations: [ BlogComponent ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      imports: [
+        HttpModule,
+        HttpClientModule,
+        FormsModule
+      ],
+        providers: [
+        ApiService
+      ]
     })
     .compileComponents();
   }));
